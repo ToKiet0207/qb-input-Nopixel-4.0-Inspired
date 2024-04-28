7e8a7d8e0b680f3d7a0e0768058899c3d3c77849
Kar <111086283+ToKiet0207@users.noreply.github.com>
2024-04-28 15:13:11 +0700

    Add files via upload

---
+++ qb-input/fxmanifest.lua
@@ -0,0 +1,23 @@
+
+
+
+fx_version "cerulean"
+game "gta5"
+
+description "qb-Input"
+version "1.2.0"
+
+client_script {
+    "client/config.lua",
+    "client/main.lua"
+}
+
+ui_page "html/index.html"
+
+files {
+    "html/index.html",
+    "html/styles/*.css",
+    "html/script.js"
+}
+
+lua54 "yes"
